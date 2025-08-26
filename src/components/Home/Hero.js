0abdc29738bcd3aabbc38bf46718f4bf69764604
essienjoy <ejoy7602@gmail.com>
2025-08-26 12:52:04 +0100

    Impliment outfit font optimization on the hero  h1 and p tag section

--- src/components/Home/Hero.js
+++ src/components/Home/Hero.js
@@ -1,17 +1,25 @@
 import Image from "next/image";
 import Link from "../UI/Link";
 import heroImg from "../../../public/images/heroimg.jpg";
+import { Outfit } from "next/font/google";
+
+const outfit = Outfit({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+  display: "swap",
+});
+
 
 const Hero = () => {
   return (
-    <section className="grid grid-rows-[1fr,minmax(200px,800px)]  lg:grid-cols-2   justify-between items-center mx-auto max-w-[var(--container-max-width)]  min-h-screen  py-5 gap-6 ">
+    <section className="grid grid-rows-[1fr,minmax(200px,800px)]   lg:grid-cols-2   justify-between items-center mx-auto max-w-[var(--container-max-width)]  min-h-screen  py-5 gap-6 ">
 
       <section className=" order-1 sm:order-2 self-start px-[var(--spacing-main-inline-padding)] flex flex-col items-start lg:order-none">
-        <h1 className="text-3xl font-bold sm:text-5xl  lg:text-[75px]">Connecting <span className="text-primary-purple">Buyers</span> and personal shoppers</h1>
-        <p className="py-6 text-lg">
+        <h1 className={`text-3xl font-bold sm:text-5xl  lg:text-[75px] ${outfit.className}`}>Connecting <span className="text-primary-purple">Buyers</span> and personal shoppers</h1>
+        <p className={`py-6 text-lg font-medium`}>
           Whether you’re buying or helping others shop, Bidbuy makes the process easy, secure, and rewarding
         </p>
-        <Link href='#' className='py-2.5 px-10 ' >View Shop</Link>
+        <Link href='#' className='py-2.5 px-10 font-semibold' >View Shop</Link>
       </section>
 
 
@@ -27,7 +35,7 @@ const Hero = () => {
         />
       </figcaption>
 
-    </section>
+    </section >
   );
 };
 
