13b5d5ade17d337d2ef56fe2d9f9f548c905c360
Osayuki <wised4536@gmail.com>
2025-08-29 17:33:00 +0100

    Hero and contact conflict resolved

--- src/components/Home/Hero.js
+++ src/components/Home/Hero.js
@@ -1,11 +1,9 @@
 import React from "react";
-import SigninForm from "../Auth/SigninForm";
 
 const Hero = () => {
   return (
     <section className="min-h-[100vh] flex items-center justify-center text-center bg-primary-purple">
       <div>Hero</div>
-      <SigninForm/>
     </section>
   );
 };
